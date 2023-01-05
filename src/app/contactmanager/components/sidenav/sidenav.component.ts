76b0dd3b9562d6c80aa133e1d7a2da360a735d9a
excellent-1 <excellentindevelopment@gmail.com>
2023-01-05 17:59:47 -0600

    Fixing the toggle between sidenav show/hide and main-content min/max width for Small screens and Large screens. Added Media Queries and Flexbox CSS

--- src/app/contactmanager/components/sidenav/sidenav.component.ts
+++ src/app/contactmanager/components/sidenav/sidenav.component.ts
@@ -1,23 +1,38 @@
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
-import { Component } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
+
+const SMALL_WIDTH_BREAKPOINT = 720;
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss']
 })
-export class SidenavComponent {
+export class SidenavComponent implements OnInit {
 
-  public isScreenSmall?: boolean;
-  constructor(private breakPointObserver: BreakpointObserver) {}
+  private mediaMatcher: MediaQueryList = 
+      matchMedia( `(max-width: ${SMALL_WIDTH_BREAKPOINT}px )`);
+
+  //public isScreenSmall?: boolean;
+
+  constructor(zone: NgZone, private breakPointObserver: BreakpointObserver) {
+    this.mediaMatcher.addListener(mql => 
+      //zone.run(() => this.mediaMatcher = mql)
+      zone.run(() => this.mediaMatcher = matchMedia(`(max-width: ${SMALL_WIDTH_BREAKPOINT}px)`))
+      );
+  }
 
   ngOnInit(): void {
-    this.breakPointObserver
+    //this.breakPointObserver
       //.observe([ `(max-width: ${SMALL_WIDTH_BREAKPOINT}px)`])
-      .observe([Breakpoints.XSmall, Breakpoints.Small, Breakpoints.Medium, Breakpoints.Large])
-      .subscribe((state: BreakpointState) => {
-        this.isScreenSmall = state.matches;
-      })
+      //.observe([Breakpoints.XSmall, Breakpoints.Small, Breakpoints.Medium, Breakpoints.Large])
+      //.subscribe((state: BreakpointState) => {
+      //  this.isScreenSmall = state.matches;
+      //})
     //.observe([Breakpoints.XSmall, Breakpoints.Small, Breakpoints.Medium, Breakpoints.Large])
+  }
+
+  isScreenSmall(): boolean {
+    return this.mediaMatcher.matches;
   }
 }
--- src/app/contactmanager/contactmanager-app.component.ts
+++ src/app/contactmanager/contactmanager-app.component.ts
@@ -9,10 +9,6 @@ import { Component } from '@angular/core';
     >>> toolbar
     >>>> main-content
     -----------------------
-      contactmanager-app works! - We are rooted to the ContactManager
-      Adding app-sidenav at the root level
-      Inside the sidenav I wired-up the app-toolbar and add router-outlet
-
       <app-sidenav></app-sidenav>
     </p>
   `,
