afff4d64c79da58a964fdfa62e6af62eda57243a
excellent-1 <excellentindevelopment@gmail.com>
2023-01-05 08:46:26 -0600

    In the Sidenav set up IsScreenSmall Breakpoint Observer - evaluates media queries against the current Viewport size, and React to changes to Viewport by Subscribing to an Observable stream. This stream will emit whenever one of the media queries has a different result.

--- src/app/contactmanager/components/sidenav/sidenav.component.ts
+++ src/app/contactmanager/components/sidenav/sidenav.component.ts
@@ -1,3 +1,4 @@
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class SidenavComponent {
 
+  public isScreenSmall?: boolean;
+  constructor(private breakPointObserver: BreakpointObserver) {}
+
+  ngOnInit(): void {
+    this.breakPointObserver
+      //.observe([ `(max-width: ${SMALL_WIDTH_BREAKPOINT}px)`])
+      .observe([Breakpoints.XSmall, Breakpoints.Small, Breakpoints.Medium, Breakpoints.Large])
+      .subscribe((state: BreakpointState) => {
+        this.isScreenSmall = state.matches;
+      })
+    //.observe([Breakpoints.XSmall, Breakpoints.Small, Breakpoints.Medium, Breakpoints.Large])
+  }
 }
