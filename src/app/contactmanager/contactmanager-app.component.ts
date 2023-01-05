75a7f956f6f0701c705d574732e3528240b08530
excellent-1 <excellentindevelopment@gmail.com>
2023-01-05 00:32:37 -0600

    The app is rooted to the ContactManager-app which contains <app-sidenav></app-sidenav>

--- src/app/contactmanager/contactmanager-app.component.ts
+++ src/app/contactmanager/contactmanager-app.component.ts
@@ -4,7 +4,16 @@ import { Component } from '@angular/core';
   selector: 'app-contactmanager-app',
   template: `
     <p>
-      contactmanager-app works!
+    > contactmanager-app 
+    >> sidenav
+    >>> toolbar
+    >>>> main-content
+    -----------------------
+      contactmanager-app works! - We are rooted to the ContactManager
+      Adding app-sidenav at the root level
+      Inside the sidenav I wired-up the app-toolbar and add router-outlet
+
+      <app-sidenav></app-sidenav>
     </p>
   `,
   styles: [
