d96ac438fdfd28a9925869e7caf24185eeae9428
excellent-1 <excellentindevelopment@gmail.com>
2023-01-10 09:49:10 -0600

    Add avatars.svg for Users to select their own avatar. Rearange UI elements. Add Save Angular Material Icon

--- src/app/contactmanager/components/new-contact-dialog/new-contact-dialog.component.ts
+++ src/app/contactmanager/components/new-contact-dialog/new-contact-dialog.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-new-contact-dialog',
   templateUrl: './new-contact-dialog.component.html',
   styleUrls: ['./new-contact-dialog.component.scss']
 })
-export class NewContactDialogComponent {
+export class NewContactDialogComponent implements OnInit {
 
-  saveNewContact() {
-    throw new Error('Method not implemented.');
-  }
+  avatars = ['svg-1','svg-2','svg-3','svg-4','svg-5', 'svg-6','svg-8','svg-9'
+    ,'svg-12','svg-13','svg-14'];
+  user?: User;
+
+  constructor(private dialogRef: MatDialogRef<NewContactDialogComponent>)
+  {}
+
+  ngOnInit(): void { this.user = new User();  }
+
+  saveNewContact() {  this.dialogRef.close(this.user);  }
+
+  dismiss() { this.dialogRef.close(null);  }
 
 }
